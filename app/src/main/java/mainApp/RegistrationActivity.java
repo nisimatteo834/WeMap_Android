2f8d25b2b4b929761d421580654d6fd199cf04dd
navidyamini <navidyamini@gmail.com>
2018-01-16 23:44:27 +0100

    creating registration page

---
+++ app/src/main/java/mainApp/RegistrationActivity.java
@@ -0,0 +1,12 @@
+package mainApp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Navid on 1/16/2018.
+ */
+
+public class RegistrationActivity extends AppCompatActivity{
+     String registraionUrl = "http://5.89.130.153/registraion.php";
+
+}
